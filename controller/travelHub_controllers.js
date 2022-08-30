f3d2219d299c1b7cfea55e45bd8b087a7cee2e99
Alyssa Wendt <alyssa95wendt@gmail.com>
2022-08-30 10:01:40 -0400

    post controller done

--- controller/travelHub_controllers.js
+++ controller/travelHub_controllers.js
@@ -22,15 +22,32 @@ router.post("/", async (req, res) => {
     const createdPost = req.body;
     // post.push(createdPost);
     try {
-      const newProduct = await db.Product.create(createdPost);
+      const newPost = await db.Post.create(createdPost);
   
-      // console.log(newProduct);
+      // console.log(newPost);
   
-      res.redirect(`/products`);
+      res.redirect(`/`);
     } catch (err) {
       console.log(err);
       res.redirect("/404");
       // throw new Error(err);
+    }
+  });
+//show route
+  router.get("/:id", async (req, res) => {
+    try {
+      const foundPost = await db.Post.findById(req.params.id);
+      const foundComment = await db.Comment.find({
+        post: req.params.id,
+      });
+      res.render("show.ejs", {
+        post: foundPost,
+        id: foundPost._id,
+        Comment: foundComment,
+      });
+    } catch (err) {
+      //console.log(err);
+      res.redirect("/404");
     }
   });
 
@@ -47,6 +64,52 @@ router.get("/", async (req, res) => {
     }
   });
 
+  //delete route
+  router.delete("/:id", async (req, res) => {
+    try {
+      const foundPost = await db.Post.findByIdAndDelete(
+        req.params.id
+      );
+      // delete any reviews where the review's product matches the resource to delete
+      const foundComment = await db.Comment.deleteMany({
+        product: req.params.id,
+      });
+      // console.log(foundPost,foundComment)
+      return res.redirect("/");
+    } catch (err) {
+      // throw new Error(err)
+      console.log(err);
+      res.redirect("/404");
+    }
+  });
+
+  // edit route
+router.get("/:id/edit", async (req, res) => {
+  
+    try {
+      const foundPost = await db.Post.findById(req.params.id);
+      // console.log(foundPost)
+      res.render("edit.ejs", { post: foundPost, id: foundPost._id });
+    } catch (err) {
+      //console.log(err);
+      res.redirect("/404");
+    }
+  });
+
+// update route 
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedPost = req.body;
+    await db.Post.findByIdAndUpdate(req.params.id, updatedPost, {
+      new: true,
+    });
+
+    res.redirect(`/${req.params.id}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/404");
+  }
+});
 
 
 module.exports = router;
