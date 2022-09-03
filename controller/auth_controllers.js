b0d407b272cd84ea7df478dd23afa26badfac5eb
maclamour <maclamour@gmail.com>
2022-09-03 09:48:47 -0400

    a

--- controller/auth_controllers.js
+++ controller/auth_controllers.js
@@ -9,7 +9,7 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
 // MODELS IMPORT
-const db = require("../models/i");
+const db = require("../models/");
 
 router.get("/login", function (req, res) {
     res.render("auth/login");
