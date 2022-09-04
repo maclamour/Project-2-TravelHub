d547855f3f463587ea85e4c079e3fb815337926d
maclamour <maclamour@gmail.com>
2022-09-04 12:22:53 -0400

    a

--- controller/travelHub_controllers.js
+++ controller/travelHub_controllers.js
@@ -10,7 +10,7 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
 // MODELS IMPORT
-const db = require("../models");
+const db = require("/models");
 
 
 //new route
